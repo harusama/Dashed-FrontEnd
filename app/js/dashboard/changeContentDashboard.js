1c108f9a2e220d13ca928f218aee06398d3b5742
Jibril <j.d.ivan69@gmail.com>
2018-03-28 06:19:53 -0600

    Added toggle for reporting options.

--- app/js/dashboard/changeContentDashboard.js
+++ app/js/dashboard/changeContentDashboard.js
@@ -2,22 +2,34 @@ $(document).ready(function () {
 
     //=================REPORT ERRORS====================
     //Change Report Error elements
+    //Toggle input report
     $('.changeErrorCard').click(function (e) {
         e.preventDefault();
         var toggleCard = $(this).attr('id');
 
+        $("#errorOptions").toggle();//Off
+        $("#errorSubmit").toggle();//On
         /*
         For question error; var == questionError;
         For system error; var == systemError;
          */
 
         if (toggleCard == "questionError"){
-            alert("Question error");
+            $("#questionReport").toggle();//On
         } else if (toggleCard == "systemError"){
-            alert("System error");
+            $("#systemReport").toggle();//On
         }else {
             alert("Error: 001");
         }
+    })
+    //Toggle report options
+    $("a.cancelReport").click(function (e) {
+        e.preventDefault();
+        $("#errorOptions").toggle();//On
+        $("#errorSubmit").toggle();//Off
+        $("#questionReport").hide();//Off
+        $("#systemReport").hide();//Off
+
     })
     //==================================================
 });
