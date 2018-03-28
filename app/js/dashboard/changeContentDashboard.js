f97c8901d831fd96f3754221c957bdd37d920e35
Jibril <j.d.ivan69@gmail.com>
2018-03-28 05:24:21 -0600

    Added partial functionality to ReportError cards.

--- app/js/dashboard/changeContentDashboard.js
+++ app/js/dashboard/changeContentDashboard.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
 
-    //Hide content from dashboard
-    $('.toggleDashboardElements').click(function() {
-        var id = '#' + $(this).attr('itemid');
-        $(id).toggle(false);
-    });
+    //=================REPORT ERRORS====================
+    //Change Report Error elements
+    $('.changeErrorCard').click(function (e) {
+        e.preventDefault();
+        var toggleCard = $(this).attr('id');
+
+        /*
+        For question error; var == questionError;
+        For system error; var == systemError;
+         */
+
+        if (toggleCard == "questionError"){
+            alert("Question error");
+        } else if (toggleCard == "systemError"){
+            alert("System error");
+        }else {
+            alert("Error: 001");
+        }
+    })
+    //==================================================
 });
