ecdcd2f44f1f46e953f5ed213b01e25e943f89a2
Jibril <j.d.ivan69@gmail.com>
2018-04-04 20:41:10 -0500

    Registration, Verification, and Login fully completed.

--- app/js/customscripts/verifyEmail.js
+++ app/js/customscripts/verifyEmail.js
@@ -1,3 +1,5 @@
+url = 'http://localhost:3000/v1/users/signup/verify?id=';
+
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results==null){
@@ -8,6 +10,68 @@ $.urlParam = function(name){
     }
 }
 
+function verifyEmail(urlHash) {
+
+    // // Request to API for signup.
+    $.ajax({
+        type: "GET",
+        url: url,
+        complete: function (xhr, settings) {
+            if (xhr.status == 200) {
+                swal({
+                    type: 'success',
+                    position: 'top-end',
+                    title: "Congratulations!",
+                    text: "Account verified!",
+                    confirmButtonText: "Go to login!",
+                    showCloseButton: true},
+                    function (isConfirm) {
+                        window.location.href = './login.html';
+                    });
+            }
+            else if (xhr.status == 400) {
+                swal({
+                    type: 'error',
+                    position: 'top-end',
+                    title: "Sorry your email has not yet been verified!",
+                    text: "Feel free to contact us if the problem persists!",
+                    confirmButtonText: "Reload",
+                    showCloseButton: true},
+                    function (isConfirm) {
+                        location.reload();
+                    });
+            }
+            else if (xhr.status == 404) {
+                swal({
+                    type: 'info',
+                    position: 'top-end',
+                    title: "Email already verified.",
+                    text: "Just login, email already verified!",
+                    confirmButtonText: "Go to login!",
+                    showCloseButton: true},
+                    function (isConfirm) {
+                        window.location.href = './login.html';
+                });
+            } else {
+                swal({
+                    type: 'error',
+                    position: 'top-end',
+                    title: "Internal error",
+                    text: "Please reload page and try again",
+                    showCancelButton: true,
+                    confirmButtonText: "Reload"
+                }, function (isConfirm) {
+                    if (isConfirm) {
+                        location.reload();
+                    }
+                });
+            }
+        }
+    });
+
+}
+
 $(document).ready(function () {
-    console.log($.urlParam('id'));
+    url += $.urlParam('id');//Get url with hash
+    verifyEmail(url);//Verifies email.
 });
