ee61535cc940ef6b3fe2a77ac87fade2e1767ae9
Jibril <j.d.ivan69@gmail.com>
2018-04-15 05:35:12 -0500

    Changed config file.

--- app/js/configuration/configuration.js
+++ app/js/configuration/configuration.js
@@ -1,7 +1,9 @@
 //Base URL
 
 //Server
-var url = 'https://dash-ed-api.herokuapp.com/v1/';
+// var url = 'https://dash-ed-api.herokuapp.com/v1/';
+var chatUrl = 'https://dash-ed-api.herokuapp.com/';
 
 //Local
-// var url = 'http://localhost:3000/';
+var url = 'http://localhost:3000/v1/';
+// var chatUrl = 'http://localhost:3000/';
