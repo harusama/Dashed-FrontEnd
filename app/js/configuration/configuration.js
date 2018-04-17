fc3482a03e7021c150e963aacc5748ff68e1b66a
Fernando López Martínez <fer.lopez94@outlook.com>
2018-04-17 18:09:22 -0500

    Add server urls

--- app/js/configuration/configuration.js
+++ app/js/configuration/configuration.js
@@ -2,12 +2,12 @@
 
 var urls = {};
 //Server
-// urls.base = 'https://dash-ed-api.herokuapp.com/v1/';
-// urls.chat = 'https://dash-ed-api.herokuapp.com/';
+urls.base = 'https://dash-ed-api.herokuapp.com/v1/';
+urls.chat = 'https://dash-ed-api.herokuapp.com/';
 
 //Local
-urls.base = 'http://localhost:3000/v1/';
-urls.chat = 'http://localhost:3000/';
+//urls.base = 'http://localhost:3000/v1/';
+//urls.chat = 'http://localhost:3000/';
 
 //Relatives
 urls.usersScope = 'users/';
